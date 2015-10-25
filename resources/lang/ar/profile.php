d91854f7e586ec2c449eeaaeaf2d186a5abd5d18
Paul Schütte <paul.schuette@mailbox.org>
2015-10-25 17:51:23 +0100

    Adds folder for arabic and german

---
+++ resources/lang/ar/profile.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'gender' => 'Geschlecht',
+    'looking for a home' => 'Ich suche eine Wohnung',
+];
---
+++ resources/lang/en/profile.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'gender' => 'gender',
+    'looking for a home' => 'I am looking for a home',
+];
